1b9c80068bc1d2000a022bd6a7ab52f2d01dcf3b
Andrea Gottardo <andrea@gottardo.me>
2018-11-22 19:22:36 -0800

    Add DNS names resolution via tooltip.

--- pagination.php
+++ pagination.php
@@ -21,7 +21,7 @@ $paginate = "ORDER BY packets.id desc LIMIT $no_of_records_per_page OFFSET $offs
     document.getElementById("pageno").innerHTML = "<?php echo $pageno; ?>";
 </script>
 
-<table class="table table-striped table-sm">
+<table class="table table-striped table-sm table-hover">
     <thead class="thead-light">
     <tr>
         <th>Packet ID</th>
@@ -71,17 +71,22 @@ $paginate = "ORDER BY packets.id desc LIMIT $no_of_records_per_page OFFSET $offs
         echo "<td>" . tool_id_to_string($row["tool"]) . "</td>";
         echo "<td>" . $row["packet_type"] . " <small><strong>" . $row["source_port"] . "→" . $row["destination_port"] . "</strong></small></td>";
         if ($srcCountry != "") {
-            echo "<td><img src='/img/flags/" . $srcCountry . ".png' height='13' /> " . $row["source_ip"] . " <small><strong><a class='text-dark' href='https://apps.db.ripe.net/db-web-ui/#/query?searchtext=".$row["source_ip"]."'>[w]</a></strong></small></td>";
+            echo "<td data-toggle=\"tooltip\" data-placement=\"top\" title='".gethostbyaddr($row["source_ip"])."'><img src='/img/flags/" . $srcCountry . ".png' height='13' /> " . $row["source_ip"] . " <small><strong><a class='text-dark' href='https://apps.db.ripe.net/db-web-ui/#/query?searchtext=".$row["source_ip"]."'>[w]</a></strong></small></td>";
         } else {
-            echo "<td>" . $row["source_ip"] . " <small><strong><a class='text-dark' href='https://apps.db.ripe.net/db-web-ui/#/query?searchtext=".$row["source_ip"]."'>[w]</a></strong></small></td>";
+            echo "<td data-toggle=\"tooltip\" data-placement=\"top\" title='".gethostbyaddr($row["source_ip"])."'>" . $row["source_ip"] . " <small><strong><a class='text-dark' href='https://apps.db.ripe.net/db-web-ui/#/query?searchtext=".$row["source_ip"]."'>[w]</a></strong></small></td>";
         }
         if ($dstCountry != "") {
-            echo "<td><img src='/img/flags/" . $dstCountry . ".png' height='15' /> " . $row["destination_ip"] . "  <small><strong><a class='text-dark' href='https://apps.db.ripe.net/db-web-ui/#/query?searchtext=".$row["destination_ip"]."'>[w]</a></strong></small></td>";
+            echo "<td data-toggle=\"tooltip\" data-placement=\"top\" title='".gethostbyaddr($row["destination_ip"])."'><img src='/img/flags/" . $dstCountry . ".png' height='15' /> " . $row["destination_ip"] . "  <small><strong><a class='text-dark' href='https://apps.db.ripe.net/db-web-ui/#/query?searchtext=".$row["destination_ip"]."'>[w]</a></strong></small></td>";
         } else {
-            echo "<td>" . $row["destination_ip"] . " <small><strong><a class='text-dark' href='https://apps.db.ripe.net/db-web-ui/#/query?searchtext=".$row["destination_ip"]."'>[w]</a></strong></small></td>";
+            echo "<td data-toggle=\"tooltip\" data-placement=\"top\" title='".gethostbyaddr($row["destination_ip"])."'>" . $row["destination_ip"] . " <small><strong><a class='text-dark' href='https://apps.db.ripe.net/db-web-ui/#/query?searchtext=".$row["destination_ip"]."'>[w]</a></strong></small></td>";
         }
         echo "</tr>";
     }
     ?>
     </tbody>
 </table>
+<script>
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+</script>
